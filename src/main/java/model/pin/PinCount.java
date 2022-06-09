c9a7add8760dce59fbb11331f63de28df1353274
cjh8746 <cjh87467@gmail.com>
2022-06-09 17:20:02 +0900

    feat : PinCount 구현

---
+++ src/main/java/model/pin/PinCount.java
@@ -0,0 +1,44 @@
+package model.pin;
+
+import java.util.Objects;
+
+public class PinCount {
+
+    private static final int MAXIMUM_PIN_COUNT = 10;
+    private static final int MINIMUM_PIN_COUNT = 0;
+
+    public final int value;
+
+    public PinCount(final int pinCount) {
+        validateSize(pinCount);
+
+        this.value = pinCount;
+    }
+
+    private void validateSize(final int pinCount) {
+        if (pinCount < MINIMUM_PIN_COUNT || MAXIMUM_PIN_COUNT < pinCount) {
+            throw new IllegalArgumentException("쓰러트린 볼링 핀의 갯수는 10을 넘을 수 없는 양수입니다.");
+        }
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public boolean isStrike() {
+        return value == MAXIMUM_PIN_COUNT;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        PinCount pinCount1 = (PinCount) o;
+        return value == pinCount1.value;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
+    }
+}
