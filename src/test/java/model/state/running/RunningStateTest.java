2de669ec177e5f8dce47629aea151dffb0aa1944
cjh8746 <cjh87467@gmail.com>
2022-06-18 20:54:22 +0900

    test : 테스트 코드 작성

---
+++ src/test/java/model/state/running/RunningStateTest.java
@@ -0,0 +1,38 @@
+package model.state.running;
+
+import model.pin.PinCount;
+import model.state.BowlingState;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class RunningStateTest {
+    private BowlingState runningState;
+
+    @BeforeEach
+    void setUp() {
+        runningState = new RunningState() {
+            @Override
+            public BowlingState bowl(PinCount knockedDownPinCount) {
+                throw new IllegalStateException("테스트 코드 중 해당 메서드를 사용하지 않습니다.");
+            }
+
+            @Override
+            public String getScoreSymbol() {
+                throw new IllegalStateException("테스트 코드 중 해당 메서드를 사용하지 않습니다.");
+            }
+        };
+    }
+
+    @Test
+    @DisplayName("투구 실행 중인 상태는 모든 투구가 끝난 상태가 아니다.")
+    void isEnd() {
+        //when
+        boolean actual = runningState.isEnd();
+
+        //then
+        assertThat(actual).isFalse();
+    }
+}
