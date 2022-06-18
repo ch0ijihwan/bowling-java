eff4b34c6ff0855d6c9d2a92601aabd86a442d76
cjh8746 <cjh87467@gmail.com>
2022-06-18 19:48:03 +0900

    test : 테스트 코드 작성

---
+++ src/test/java/model/state/ended/MissTest.java
@@ -0,0 +1,56 @@
+package model.state.ended;
+
+import model.pin.PinCount;
+import model.state.BowlingState;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalStateException;
+
+class MissTest {
+
+    private BowlingState miss;
+
+    @BeforeEach
+    void setUp() {
+        PinCount firstPinCount = new PinCount(3);
+        PinCount secondPinCount = new PinCount(5);
+        miss = Miss.create(firstPinCount, secondPinCount);
+    }
+
+    @Test
+    @DisplayName("Miss 상태는 더이상 투구를 할 수 없다.")
+    void bowl() {
+        //given
+        PinCount pinCount = new PinCount(1);
+
+        //when
+        assertThatIllegalStateException().isThrownBy(() -> miss.bowl(pinCount))
+                .withMessage("이미 투구가 끝난 상태 입니다.");
+    }
+
+    @Test
+    @DisplayName("미스에 해당하는 핀의 갯수 기호를 반환한다.")
+    void getScoreSymbol() {
+        //given
+        String expect = "3|5";
+
+        //when
+        String actual = miss.getScoreSymbol();
+
+        //then
+        assertThat(actual).isEqualTo(expect);
+    }
+
+    @Test
+    @DisplayName("Miss 는 모든 투구가 끝난 상태이다.")
+    void isEnd() {
+        //when
+        boolean actual= miss.isEnd();
+
+        //then
+        assertThat(actual).isTrue();
+    }
+}
