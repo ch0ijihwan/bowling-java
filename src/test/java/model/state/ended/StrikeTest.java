c275c4b1696894c5fe758fbdf6855c5f9f900897
cjh8746 <cjh87467@gmail.com>
2022-06-18 19:03:26 +0900

    test : Strike 및 EndedState 테스트 코드 작성

---
+++ src/test/java/model/state/ended/StrikeTest.java
@@ -0,0 +1,52 @@
+package model.state.ended;
+
+import model.pin.PinCount;
+import model.state.BowlingState;
+import model.state.ended.Strike;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalStateException;
+
+class StrikeTest {
+
+    @Test
+    @DisplayName("스트라이크 기호를 반환한다.")
+    void getSymbol() {
+
+        //given
+        BowlingState strike = Strike.create();
+        String expect = " X ";
+
+        //when
+        String actual = strike.getScoreSymbol();
+
+        //then
+        assertThat(actual).isEqualTo(expect);
+    }
+
+    @Test
+    @DisplayName("스트라이크 인 경우 두번 째 투구가 불가능하다.")
+    void bowl() {
+        //given
+        BowlingState strike = Strike.create();
+        PinCount pinCount = new PinCount(1);
+
+        //when
+        assertThatIllegalStateException().isThrownBy(() -> strike.bowl(pinCount))
+                .withMessage("이미 투구가 끝난 상태 입니다.");
+    }
+
+    @Test
+    @DisplayName("스트라이크 상태는 투구가 끝난 상태이다.")
+    void isEnd() {
+        //given
+        BowlingState strike = Strike.create();
+
+        //when
+        boolean actual = strike.isEnd();
+        //then
+        assertThat(actual).isTrue();
+    }
+}
