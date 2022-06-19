4309a2f853c675c795cea4435b1a693ed907812f
cjh8746 <cjh87467@gmail.com>
2022-06-19 16:18:57 +0900

    test : Player 테스트 코드 작성

---
+++ src/test/java/model/PlayerTest.java
@@ -0,0 +1,35 @@
+package model;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalStateException;
+import static org.junit.jupiter.api.Assertions.*;
+
+class PlayerTest {
+
+    @Test
+    @DisplayName("플레이어의 이름을 반환한다.")
+    void getPlayerName() {
+        //given
+        Player player = new Player("ABC");
+
+        //when
+        String actual = player.getPlayerName();
+
+        //then
+        assertThat(actual).isEqualTo("ABC");
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"abc","123","A"})
+    @DisplayName("플레이어의 이름은 3글자이며 대문자로 구성되어있다.")
+    void validatePlayerName(final String name) {
+        //when
+        assertThatIllegalStateException().isThrownBy(() -> new Player(name))
+                .withMessage("플레이어의 이름은 알파벳 대문자로 구성된 3자리 이니셜 입니다.");
+    }
+}
