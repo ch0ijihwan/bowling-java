dc05780b1ce1aa5ff68fec53e96d6036659624d8
cjh8746 <cjh87467@gmail.com>
2022-06-19 13:32:03 +0900

    style : 불필요한 줄 띄움 제거

---
+++ src/main/java/model/frame/LastFrame.java
@@ -0,0 +1,101 @@
+package model.frame;
+
+import model.pin.PinCount;
+import model.state.BowlingState;
+import model.state.ended.Gutter;
+import model.state.ended.Miss;
+import model.state.ended.Spare;
+import model.state.ended.Strike;
+import model.state.running.FirstPitch;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class LastFrame implements Frame {
+
+    private static final int MAXIMUM_BOWL_COUNT = 3;
+    private static final int MINIMUM_BOWL_COUNT = 2;
+    private static final int LAST_FRAME_INDEX = 10;
+
+    private final List<BowlingState> states = new ArrayList<>();
+    private int bowlCount;
+
+    private LastFrame() {
+        this.states.add(FirstPitch.create());
+        this.bowlCount = 0;
+    }
+
+    public static Frame create() {
+        return new LastFrame();
+    }
+
+    @Override
+    public Frame bowl(final PinCount pinCount) {
+        BowlingState current = getLastState();
+
+        if (isStrikeOrSpare(current)) {
+            bonusBowl(pinCount);
+            return this;
+        }
+
+        states.remove(getStatesLastIndex());
+        states.add(current.bowl(pinCount));
+        bowlCount++;
+        return this;
+    }
+
+    private void bonusBowl(final PinCount pinCount) {
+        BowlingState bonusBowl = FirstPitch.create()
+                .bowl(pinCount);
+        states.add(bonusBowl);
+        bowlCount++;
+    }
+
+    private int getStatesLastIndex() {
+        return states.size() - 1;
+    }
+
+    private BowlingState getLastState() {
+        return states.get(getStatesLastIndex());
+    }
+
+    private boolean isStrikeOrSpare(final BowlingState current) {
+        return current instanceof Strike || current instanceof Spare;
+    }
+
+    @Override
+    public int getIndex() {
+        return LAST_FRAME_INDEX;
+    }
+
+    @Override
+    public boolean isEnd() {
+        if (bowlCount == MAXIMUM_BOWL_COUNT) {
+            return true;
+        }
+        BowlingState lastState = getLastState();
+        return isDoubleStrike() || lastState instanceof Miss || lastState instanceof Gutter;
+    }
+
+    private boolean isDoubleStrike() {
+        return states.stream()
+                .limit(MINIMUM_BOWL_COUNT)
+                .allMatch(Strike.class::isInstance);
+    }
+
+    @Override
+    public String getScoreSymbol() {
+        return states.stream()
+                .map(BowlingState::getScoreSymbol)
+                .collect(Collectors.joining("|"));
+    }
+
+    @Override
+    public String toString() {
+        return "LastFrame{" +
+                "states=" + states +
+                ", bowlCount=" + bowlCount +
+                '}';
+    }
+}
