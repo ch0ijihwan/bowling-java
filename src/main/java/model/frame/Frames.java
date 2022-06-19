34d5429bc92cbc3b45f0833cded2ad5175d89b6f
cjh8746 <cjh87467@gmail.com>
2022-06-19 15:13:25 +0900

    feat : Frames 구현

---
+++ src/main/java/model/frame/Frames.java
@@ -0,0 +1,47 @@
+package model.frame;
+
+import model.pin.PinCount;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Frames {
+
+    private final List<Frame> frames = new ArrayList<>();
+
+    public static Frames createFirst() {
+        return new Frames();
+    }
+
+    private Frames() {
+        this.frames.add(NormalFrame.createFirst());
+    }
+
+    public int getLastFrameIndex() {
+        return getLastPositionFrame().getIndex();
+    }
+
+    private Frame getLastPositionFrame() {
+        return frames.get(getLastIndex());
+    }
+
+    private int getLastIndex() {
+        return frames.size() - 1;
+    }
+
+    public void bowl(final PinCount pinCount) {
+        Frame currentFrame = getLastPositionFrame();
+        Frame resultFrame = currentFrame.bowl(pinCount);
+        if (currentFrame.isEnd() && !resultFrame.isEnd()) {
+            frames.add(resultFrame);
+        }
+    }
+
+    public List<Frame> getFrames() {
+        return new ArrayList<>(this.frames);
+    }
+
+    public boolean hasNextPitching() {
+        return !(getLastPositionFrame().isEnd() && frames.size() == 10);
+    }
+}
