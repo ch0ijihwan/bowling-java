3fb1dcb15dbb0517b3d9d99067c3e0a3807aac02
cjh8746 <cjh87467@gmail.com>
2022-06-24 18:57:01 +0900

    test : 정적팩토리 메서드 추가를 위한 test 코드 수정

--- src/test/java/model/frame/ScoreTest.java
+++ src/test/java/model/frame/ScoreTest.java
@@ -2,9 +2,13 @@ package model.frame;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatIllegalStateException;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.*;
 
 class ScoreTest {
 
@@ -12,7 +16,7 @@ class ScoreTest {
     @DisplayName("투구 실행 시, 투구 실행 후, 점수를 더한 새로운 Score 객체를 생성해 반환한다. 이때, 보너스 기회가 있을 경우 보너스 기회를 하나 줄인다. ")
     void bowl() {
         //given
-        Score oneStrikeScore = new Score(10, 2);
+        Score oneStrikeScore = Score.createStrikeScore();
 
         //when
         Score actual = oneStrikeScore.bowl(3);
@@ -26,24 +30,96 @@ class ScoreTest {
     @DisplayName("점수를 반환한다.")
     void getScore() {
         //given
-        Score score = new Score(10, 2).bowl(5).bowl(1);
-        int expect =  16;
-        
+        Score score = Score.createStrikeScore().bowl(5).bowl(1);
+        int expect = 16;
+
         //when
         int actual = score.getScore();
 
         //then
         assertThat(actual).isEqualTo(expect);
     }
-    
+
     @Test
     @DisplayName("보너스 점수 기회 횟수가 남아 있는 경우, 점수를 확인 할 수 없다.")
     void validateRemainingBonusCount() {
         //given
-        Score score = new Score(10, 2).bowl(5);
+        Score score = Score.createStrikeScore().bowl(5);
 
         //when
-        assertThatIllegalStateException().isThrownBy(() -> score.getScore())
+        assertThatIllegalStateException().isThrownBy(score::getScore)
                 .withMessage("추가 득점 기회가 남아 있어, 점수를 확인 할 수 없습니다.");
+    }
+
+    @Test
+    @DisplayName("스트라이크 점수를 생성한다.")
+    void createStrikeScore() {
+        //given
+        int expectScore = 10;
+        int expectRemainingBonusCount = 2;
+
+        //when
+        Score score = Score.createStrikeScore();
+
+        //then
+        assertThat(score).hasFieldOrPropertyWithValue("score", expectScore)
+                .hasFieldOrPropertyWithValue("remainingBonusCount", expectRemainingBonusCount);
+    }
+
+    @Test
+    @DisplayName("스페어 점수를 생성한다.")
+    void createSpareScore() {
+        //given
+        int expectScore = 10;
+        int expectRemainingBonusCount = 1;
+
+        //when
+        Score score = Score.createSpareScore();
+
+        //then
+        assertThat(score).hasFieldOrPropertyWithValue("score", expectScore)
+                .hasFieldOrPropertyWithValue("remainingBonusCount", expectRemainingBonusCount);
+    }
+
+    @Test
+    @DisplayName("미스 상태의 점수를 생성한다.")
+    void createMissScore() {
+        //given
+        int input = 9;
+        int expectScore = 9;
+        int expectRemainingBonusCount = 0;
+
+        //when
+        Score score = Score.createMissScore(input);
+
+        //then
+        assertThat(score).hasFieldOrPropertyWithValue("score", expectScore)
+                .hasFieldOrPropertyWithValue("remainingBonusCount", expectRemainingBonusCount);
+    }
+
+    @Test
+    @DisplayName("미스 상태의 점수는 10 이상 일 수 없다.")
+    void validateMissScore() {
+        //given
+        int input = 10;
+
+        assertThatIllegalArgumentException().isThrownBy(() -> Score.createMissScore(input))
+                .withMessage("미스 상태의 점수는 10 이상 일 수 없습니다.");
+    }
+
+    @ParameterizedTest
+    @DisplayName("보너스 점수 기회가 남아 있으면 true 를 반환한다.")
+    @MethodSource("scoreParameterProvider")
+    void hasRemainingBonusCount(final Score input, final boolean expect) {
+        //when
+        boolean actual = input.hasRemainingBonusCount();
+
+        //then
+        assertThat(actual).isEqualTo(expect);
+    }
+
+    private static Stream<Arguments> scoreParameterProvider() {
+        return Stream.of(Arguments.of(Score.createSpareScore(), true),
+                Arguments.of(Score.createMissScore(1), false));
     }
 }
