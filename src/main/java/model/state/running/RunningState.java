f6d4df38feff893f01fd3809932e50f02c6b7805
cjh8746 <cjh87467@gmail.com>
2022-07-01 17:06:03 +0900

    refactor : 예외처리 메시지 변경

--- src/main/java/model/state/running/RunningState.java
+++ src/main/java/model/state/running/RunningState.java
@@ -18,6 +18,6 @@ public abstract class RunningState implements BowlingState {
 
     @Override
     public Score createScore() {
-        throw new NotCountScore("투구 실행 중에는 점수를 확인 할 수 없습니다.");
+        throw new NotCountScore("투구 실행 중에는 점수를 계산 할 수 없습니다.");
     }
 }
