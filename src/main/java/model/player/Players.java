1818f77359874127fa0100a6bec0c2f9ecf4de25
cjh8746 <cjh87467@gmail.com>
2022-07-01 17:28:24 +0900

    feat : Players 구현

---
+++ src/main/java/model/player/Players.java
@@ -0,0 +1,16 @@
+package model.player;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Players {
+    private final List<Player> players;
+
+    public Players(final List<Player> players) {
+        this.players = players;
+    }
+
+    public List<Player> getPlayers() {
+        return new ArrayList<>(players);
+    }
+}
