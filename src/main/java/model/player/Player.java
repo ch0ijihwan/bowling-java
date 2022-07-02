a69b3be43fe0a957658954fcb30c6c7dc60b1c25
cjh8746 <cjh87467@gmail.com>
2022-07-02 23:36:57 +0900

    refactor : 해당하는 위치의 프레임의 투구가 끝났는지 확인하는 로직 생성

--- src/main/java/model/player/Player.java
+++ src/main/java/model/player/Player.java
@@ -39,4 +39,10 @@ public class Player {
     public int hashCode() {
         return Objects.hash(name);
     }
+
+    public boolean didBowlingEndAt(final int index) {
+        return frames.getFrames()
+                .get(index)
+                .isEnd();
+    }
 }
